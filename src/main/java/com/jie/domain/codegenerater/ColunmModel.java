12dd06415bb3089c3dd8271d3df5e242ac338337
liuxiaojie <xiaojie_liu@ziguangcn.com>
2020-04-21 14:04:44 +0800

    添加lombok以来

--- src/main/java/com/jie/domain/codegenerater/ColunmModel.java
+++ src/main/java/com/jie/domain/codegenerater/ColunmModel.java
@@ -1,5 +1,8 @@
 package com.jie.domain.codegenerater;
 
+import lombok.Data;
+
+@Data
 public class ColunmModel {
 	/**
 	 * 列名
@@ -22,43 +25,4 @@ public class ColunmModel {
 	 */
 	private String notes;
 
-	public String getPropertyName() {
-		return propertyName;
-	}
-
-	public void setPropertyName(String propertyName) {
-		this.propertyName = propertyName;
-	}
-
-	public String getPropertyType() {
-		return propertyType;
-	}
-
-	public void setPropertyType(String propertyType) {
-		this.propertyType = propertyType;
-	}
-
-	public String getNotes() {
-		return notes;
-	}
-
-	public void setNotes(String notes) {
-		this.notes = notes;
-	}
-
-	public String getColumName() {
-		return columName;
-	}
-
-	public void setColumName(String columName) {
-		this.columName = columName;
-	}
-
-	public String getPrefixUppercasePropertyName() {
-		return prefixUppercasePropertyName;
-	}
-
-	public void setPrefixUppercasePropertyName(String prefixUppercasePropertyName) {
-		this.prefixUppercasePropertyName = prefixUppercasePropertyName;
-	}
 }
--- src/main/java/com/jie/domain/codegenerater/TemplateDataModel.java
+++ src/main/java/com/jie/domain/codegenerater/TemplateDataModel.java
@@ -1,7 +1,10 @@
 package com.jie.domain.codegenerater;
 
+import lombok.Data;
+
 import java.util.List;
 
+@Data
 public class TemplateDataModel {
 	/**
 	 * 包路径
@@ -43,83 +46,4 @@ public class TemplateDataModel {
 	 * condition类所在相对路径
 	 */
 	private String conditionPath;
-	public String getPackagePath() {
-		return packagePath;
-	}
-
-	public void setPackagePath(String packagePath) {
-		this.packagePath = packagePath;
-	}
-
-	public String getClassName() {
-		return className;
-	}
-
-	public void setClassName(String className) {
-		this.className = className;
-	}
-
-	public List<ColunmModel> getColunmModels() {
-		return colunmModels;
-	}
-
-	public void setColunmModels(List<ColunmModel> colunmModels) {
-		this.colunmModels = colunmModels;
-	}
-
-	public List<String> getImportClassPaths() {
-		return importClassPaths;
-	}
-
-	public void setImportClassPaths(List<String> importClassPaths) {
-		this.importClassPaths = importClassPaths;
-	}
-
-	public String getPrefixLowercaseClassName() {
-		return prefixLowercaseClassName;
-	}
-
-	public void setPrefixLowercaseClassName(String prefixLowercaseClassName) {
-		this.prefixLowercaseClassName = prefixLowercaseClassName;
-	}
-
-	public String getUppercaseClassName() {
-		return uppercaseClassName;
-	}
-
-	public void setUppercaseClassName(String uppercaseClassName) {
-		this.uppercaseClassName = uppercaseClassName;
-	}
-
-	public String getTableName() {
-		return tableName;
-	}
-
-	public void setTableName(String tableName) {
-		this.tableName = tableName;
-	}
-
-	public String getNamespace() {
-		return namespace;
-	}
-
-	public void setNamespace(String namespace) {
-		this.namespace = namespace;
-	}
-
-	public String getDomainPath() {
-		return domainPath;
-	}
-
-	public void setDomainPath(String domainPath) {
-		this.domainPath = domainPath;
-	}
-
-	public String getConditionPath() {
-		return conditionPath;
-	}
-
-	public void setConditionPath(String conditionPath) {
-		this.conditionPath = conditionPath;
-	}
 }
